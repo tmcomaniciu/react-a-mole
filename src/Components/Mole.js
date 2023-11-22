aa21b6cb18972d4e21e6f5c0379c70d7b59da13f
Tudor Comaniciu <tmcomaniciu@gmail.com>
2023-11-22 15:31:27 -0500

    initial commit

---
+++ src/Components/Mole.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import moleImg from "../mole.png";
+
+const Mole = (props) => {
+  useEffect(() => {
+    let randSeconds = Math.ceil(Math.random() * 10000);
+    let timer = setTimeout(() => {
+      props.toggle(false);
+    }, randSeconds);
+    return () => clearTimeout(timer);
+  });
+
+  return (
+    <div>
+      <img
+        style={{ width: "30vw" }}
+        src={moleImg}
+        alt="mole"
+        onClick={props.handleClick}
+      />
+    </div>
+  );
+};
+
+export default Mole;
